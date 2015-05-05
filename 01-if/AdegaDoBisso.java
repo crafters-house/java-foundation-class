7206e2af9b65e62a35d8bc2f74cd15e31463c659
Eduardo-Bisso <ercarval@gmail.com>
2015-05-04 23:12:57 -0300

    initial import

---
+++ 01-if/AdegaDoBisso.java
@@ -0,0 +1,36 @@
+﻿/*
+ * if (condicao) {} 
+ *
+ * A Adega do Bisso precisa garantir que as cervejas 
+ * não seja vendidas para menores de 18 anos.
+ * ele tmb precisa saber quanto foi o valor Recebido pela Venda.
+ *
+ *   
+ */   
+class AdegaDoBisso {
+   
+   // Não vender Bebidas Alcoolicas para Crianças
+   // Vender Refrigerante pode!!
+   // Imprimir o valor Vendido     	
+
+   public static void main (String[] args) {
+       
+       int idadeCliente = 16;
+
+       int quantidadeCerveja = 3;
+		
+       double valorCerveja = 6.0;		
+
+       double valorRebebido = 0.0; 		
+
+       if (idadeCliente >= 18) {
+          valorRecebido = valorCerveja * quantidadeCerveja;   
+       }    	
+     
+       	   		
+        
+     
+   }	
+
+
+}
---
+++ 02-switch/SaldoContasBancarias.java
@@ -0,0 +1,20 @@
+/**
+ * Um cliente pediu para o Gerente do Banco informar
+ * o saldo de suas contas : 
+ *   Conta Corrente - 'C' : (2000)
+ *   Conta Poupanca - 'P' : (1500.45)
+ *   Conta CDB      - 'I' : (500.78)
+ *   Conta Fundo de Ações - 'F' : (6500.81)
+ *   
+ */  
+class SaldoContasBancarias {
+	
+    public static void main (String[] args) {
+ 	 
+        char tipoConta = 'I'; 
+       
+    								
+	          
+    }
+
+}
